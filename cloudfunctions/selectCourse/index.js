fe044907b25bc320f2703dbdbcf021363229399a
yzx <1727718977@qq.com>
2019-07-09 10:39:09 +0800

    加入容量限制

--- cloudfunctions/selectCourse/index.js
+++ cloudfunctions/selectCourse/index.js
@@ -7,16 +7,16 @@ const _ = db.command
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  await db.collection('userinfo').where({
-    _openid:event.openid,
-  }).update({
-    data:{
-      projectName: event.projectName
-    }
-  });
-  await db.collection('projects').where({projectName:event.projectName,}).update({
+  return await db.collection('projects').where({projectName:event.projectName,num:_.gt(0)}).update({
     data:{
       num:_.inc(-1)
+    }
+  })
+  await db.collection('userinfo').where({
+    _openid: event.openid,
+  }).update({
+    data: {
+      projectName: event.projectName
     }
   })
 }
